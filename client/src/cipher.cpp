39cbeccc9a92fe852d5885e7da4431b71d985017
Sergeyais <115755575+Sergeyais@users.noreply.github.com>
2023-06-07 21:23:43 +0300

    create key.txt with password

--- client/src/cipher.cpp
+++ client/src/cipher.cpp
@@ -2,12 +2,38 @@
 #include <iostream>
 #include <string>
 #include <cstring>
+#include <fstream>
 
+
+const unsigned char* getkey() {
+    std::ifstream file("key.txt"); // открываем файл для чтения
+    std::string line; // переменная для хранения строки
+
+    if (file.is_open()) { // проверяем, удалось ли открыть файл
+        if (std::getline(file, line)) { // считываем первую строку из файла
+            
+            // Преобразование строки в массив типа const unsigned char[]
+            static const unsigned char key[] = { line.c_str()[0], line.c_str()[1], line.c_str()[2], '\0' };
+
+            return key;
+        } else {
+            std::cout << "1" << std::endl;
+        }
+        file.close(); // закрываем файл
+    } else {
+        std::cout << "2" << std::endl;
+    }
+
+    // Если чтение файла не удалось, можно вернуть значение по умолчанию
+    static const unsigned char defaultKey[] = { '\0' };
+    return defaultKey;
+}
 
 void encrypt_AES(char* plaintext, size_t length)
 {
     // Ключ для шифрования
-    const unsigned char key[] = "0123456789abcdef";
+    // const unsigned char key[] = "0123456789abcdef";
+    const unsigned char *key = getkey();
 
     AES_KEY aesKey;
     AES_set_encrypt_key(key, 128, &aesKey); 
@@ -24,7 +50,8 @@ void encrypt_AES(char* plaintext, size_t length)
 void decrypt_AES(char* ciphertext, size_t length)
 {
     // Ключ для дешифрования
-    const unsigned char key[] = "0123456789abcdef";
+    // const unsigned char key[] = "0123456789abcdef";
+    const unsigned char *key = getkey();
 
     AES_KEY aesKey;
     AES_set_decrypt_key(key, 128, &aesKey);
